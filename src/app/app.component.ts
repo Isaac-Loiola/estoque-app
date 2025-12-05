37d9c19a8c1453ae0e6c20bae2e4fe73f92c68c8
Isaac Loiola <isaackopi@icloud.com>
2025-12-05 19:18:29 -0300

    Estilização tab-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { albums, home, layers } from 'ionicons/icons';
+import { addCircle, albums, home, layers } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,6 @@ import { albums, home, layers } from 'ionicons/icons';
 })
 export class AppComponent {
   constructor() {
-    addIcons({albums, layers, home})
+    addIcons({albums, layers, home, addCircle})
   }
 }
